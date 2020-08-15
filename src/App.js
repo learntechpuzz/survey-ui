a17edf740eca22646005d5fc08541a6db5c7b6bf
learntechpuzz <learntechpuzz@gmail.com>
2020-08-15 20:07:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { StylesProvider, jssPreset, ThemeProvider } from '@material-ui/styles';
+import theme from './assets/theme';
+import { Typography, FormControl, Select, MenuItem } from '@material-ui/core';
+
+// Configure JSS
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 
 function App() {
+  const [lang, changeLanguage] = React.useState('ltr');
+  const handleChangeLanguage = (event) => {
+    let dir = event.target.value;
+    changeLanguage(dir);
+    document.querySelector("#root").setAttribute("dir", dir);
+    theme.direction = dir;
+}  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <StylesProvider jss={jss}>
+      <div>
+        <FormControl>
+          <Select id="lang" value={lang} onChange={ handleChangeLanguage }>
+            <MenuItem value="ltr">English</MenuItem>
+            <MenuItem value="rtl">Arabic</MenuItem>
+          </Select>
+        </FormControl>
+        <Typography>Hello World!</Typography>  
+      </div>
+    </StylesProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/assets/theme.js
@@ -0,0 +1,8 @@
+import {createMuiTheme} from '@material-ui/core/styles';
+
+var dir = "ltr";
+const theme = createMuiTheme({
+    direction: dir,
+  });
+
+export default theme;
